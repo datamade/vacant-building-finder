5cbd083d82e505ccb5c0121956dc3f4b2b42e3f5
Derek Eder <derek.eder@gmail.com>
2011-09-29 01:13:37 -0500

    added date range slider, compressed search filters, reworded intro, added census legend

--- source/maps_lib.js
+++ source/maps_lib.js
@@ -6,7 +6,7 @@
   var map;
   var geocoder;
   var addrMarker;
-  var addrMarkerImage = 'http://chicagobuildings.org/images/icons/blue-pushpin.png';
+  var addrMarkerImage = 'http://chicagobuildings.org/images/blue-pushpin.png';
   
   var fusionTableId = 1614852; //main table for building data
   
@@ -55,21 +55,41 @@
 	$("#cbVacant2").attr("checked", "checked");
 	$("#cbVacant3").attr("checked", "checked");
 	
-	$("#cbInUse1").attr("checked", "checked");
-	$("#cbInUse2").attr("checked", "checked");
-	
 	$("#cbOpen1").attr("checked", "checked");
 	$("#cbOpen2").attr("checked", "checked");
 	$("#cbOpen3").attr("checked", "checked");
-	
-	$("#cbFire1").attr("checked", "checked");
-	$("#cbFire2").attr("checked", "checked");
 
 	searchBuildings = null;
 	
 	poverty.setMap(map);
-	buildings.setMap(map);
+	doSearch();
 	$("#txtSearchAddress").val("");
+  }
+  
+  function initializeDateSlider() {
+	var minDate = new Date(2010, 1-1, 1);
+    var maxDate = new Date();
+    var initialStartDate = new Date();
+    initialStartDate.setDate(maxDate.getDate() - 180);
+    $('#startDate').html($.datepicker.formatDate('mm/dd/yy', initialStartDate));
+    $('#endDate').html($.datepicker.formatDate('mm/dd/yy', maxDate));
+    $('#date-range').slider({
+    	range: true,
+    	step: 30,
+    	values: [ Math.floor((initialStartDate.getTime() - minDate.getTime()) / 86400000), Math.floor((maxDate.getTime() - minDate.getTime()) / 86400000) ],
+        max: Math.floor((maxDate.getTime() - minDate.getTime()) / 86400000),
+        slide: function(event, ui) {
+            var date = new Date(minDate.getTime());
+            date.setDate(date.getDate() + ui.values[0]);
+            $('#startDate').html($.datepicker.formatDate('mm/dd/yy', date));
+            date = new Date(minDate.getTime());
+            date.setDate(date.getDate() + ui.values[1]);
+            $('#endDate').html($.datepicker.formatDate('mm/dd/yy', date));
+        },
+        stop: function(event, ui) {
+        	doSearch();
+        }
+    });
   }
 	
 	function doSearch() 
@@ -83,17 +103,14 @@
 		var vacant2 = $("#cbVacant2").is(':checked');
 		var vacant3 = $("#cbVacant3").is(':checked');
 		
-		var inUse1 = $("#cbInUse1").is(':checked');
-		var inUse2 = $("#cbInUse2").is(':checked');
-		
 		var open1 = $("#cbOpen1").is(':checked');
 		var open2 = $("#cbOpen2").is(':checked');
 		var open3 = $("#cbOpen3").is(':checked');
 		
+		var inUse1 = $("#cbInUse1").is(':checked');
 		var fire1 = $("#cbFire1").is(':checked');
-		var fire2 = $("#cbFire2").is(':checked');
 		
-		searchStr = "SELECT LONGITUDE FROM " + fusionTableId + " WHERE LONGITUDE not equal to ''";
+		searchStr = "SELECT 'Full Address' FROM " + fusionTableId + " WHERE 'Full Address' not equal to ''";
 		
 		//vacant
 		var searchVacant = "'Vacant flag' IN (-1,";
@@ -106,15 +123,6 @@
 
         searchStr += " AND " + searchVacant.slice(0, searchVacant.length - 1) + ")";
 		
-		//in use
-		var searchUse = "'In use flag' IN (-1,";
-        if (inUse1)
-			searchUse += "1,";
-		if (inUse2)
-			searchUse += "0,";
-
-        searchStr += " AND " + searchUse.slice(0, searchUse.length - 1) + ")";
-		
 		//is open
 		var searchOpen = "'Open flag' IN (-1,";
         if (open1)
@@ -126,14 +134,16 @@
 
         searchStr += " AND " + searchOpen.slice(0, searchOpen.length - 1) + ")";
 		
+		//in use
+        if (inUse1)
+			searchStr += " AND 'In use flag' = 1";
+		
 		//fire
-		var searchFire = "'Fire flag' IN (-1,";
         if (fire1)
-			searchFire += "1,";
-		if (fire2)
-			searchFire += "0,";
-
-        searchStr += " AND " + searchFire.slice(0, searchFire.length - 1) + ")";
+			searchStr += " AND 'Fire flag' = 1";
+        
+        searchStr += " AND 'DATE RECEIVED' >= '" + $('#startDate').html() + "'";
+        searchStr += " AND 'DATE RECEIVED' <= '" + $('#endDate').html() + "'";
 		
 		// because the geocode function does a callback, we have to handle it in both cases - when they search for and address and when they dont
 		if (address != "")
@@ -158,9 +168,10 @@
 				});
 				drawSearchRadiusCircle(results[0].geometry.location);
 				
-				searchStr += " AND ST_INTERSECTS(LONGITUDE, CIRCLE(LATLNG" + results[0].geometry.location.toString() + "," + searchRadius + "))";
+				searchStr += " AND ST_INTERSECTS('Full Address', CIRCLE(LATLNG" + results[0].geometry.location.toString() + "," + searchRadius + "))";
 				
 				//get using all filters
+				console.log(searchStr);
 				searchBuildings = new google.maps.FusionTablesLayer(fusionTableId, {
 					query: searchStr}
 					);
@@ -177,6 +188,7 @@
 		else
 		{
 			//get using all filters
+			console.log(searchStr);
 			searchBuildings = new google.maps.FusionTablesLayer(fusionTableId, {
 				query: searchStr}
 				);
@@ -278,22 +290,10 @@
 	
 	function displayCount(searchStr) {
 	  //set the query using the parameter
-	  searchStr = searchStr.replace('SELECT LONGITUDE ','SELECT Count() ');
+	  searchStr = searchStr.replace("SELECT 'Full Address' ","SELECT Count() ");
 	  
 	  //set the callback function
 	  getFTQuery(searchStr).send(displaySearchCount);
-	}
-	
-	function getTotalRecordCount() {
-		getFTQuery("SELECT Count() FROM " + fusionTableId).send(displayTotalRecordCount);
-	}
-	
-	function displayTotalRecordCount(response) {
-	  var count = 0;
-	  if (response.getDataTable().getNumberOfRows() > 0)
-	  	count = parseInt(response.getDataTable().getValue(0, 0));
-	  
-	  $('#totalBuildings').html(addCommas(count));	
 	}
 
 	function displaySearchCount(response) {
@@ -304,7 +304,7 @@
 	  if (numRows == 1)
 		name = recordName;
 	  $( "#resultCount" ).fadeOut(function() {
-        $( "#resultCount" ).html("found " + numRows + " " + name);
+        $( "#resultCount" ).html(addCommas(numRows) + " " + name + " found");
       });
 	  $( "#resultCount" ).fadeIn();
 	}
